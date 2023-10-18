d6d245d42781848f76081543c40dcc35c641c5ad
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-10-18 20:17:06 +0900

    fix:モデルとオプティマイザの定義

--- src/Split_Server.py
+++ src/Split_Server.py
@@ -1,7 +1,29 @@
-epochs = 5
-
 import torch
 import torchvision import datasets, transforms
 from torch import nn, optim
 from torchvision.datasets import MNIST
 from torchvision.trasnforms import ToTensor
+
+epochs = 5
+input_size = 784
+hidden_sizes = [128, 640]
+output_size = 10
+
+models = [
+    nn.Sequential(
+        nn.Linear(input_size, hidden_sizes[0]),
+        nn.ReLU(),
+        nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+        nn.ReLU()
+    ),
+    nn.Sequential(
+        nn.Linear(hidden_sizes[1], output_size),
+        nn.LogSoftmax(dim=1)
+    ),
+]
+
+optimizers = [
+    optim.SGD(model.parameters(), lr=0.003,)
+    for model in models
+]
+
