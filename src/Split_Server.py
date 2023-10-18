99b5d26372e82cf48209819f7df65265b049e5ab
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-10-18 19:59:56 +0900

    add:MNISTデータでSLするための大元ファイル追加

---
+++ src/Split_Server.py
@@ -0,0 +1,7 @@
+epochs = 5
+
+import torch
+import torchvision import datasets, transforms
+from torch import nn, optim
+from torchvision.datasets import MNIST
+from torchvision.trasnforms import ToTensor
