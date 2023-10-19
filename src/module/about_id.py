4b9e7dd40a0c9a5e0f73c30f1f1d67531c4539fc
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-10-19 20:56:12 +0900

    [feat] 同じindexのデータとラベルに同じIDを付与するように実装

--- src/module/about_id.py
+++ src/module/about_id.py
@@ -1,6 +1,26 @@
+import torch
+import numpy as np
+
 def add_ids(data, label):
+    #データとラベルのサイズが異なる場合はエラーを返す
     if len(data) != len(label):
         raise RuntimeError(">> Error: data and label are not same length.")
-        
+    
+    print("---Adding IDs to data and label---")
+    #データサイズ分のランダムなIDを生成
+    ids = np.arange(len(data))
+    np.random.shuffle(ids)
+
+    new_data = [[] for _ in range(len(data))]
+    new_label = [[] for _ in range(len(data))]
+    
+    for i in range(len(data)):
+        new_data[i].append(data[i])
+        new_data[i].append(ids[i])
+        new_label[i].append(label[i])
+        new_label[i].append(ids[i])
+
+    if len(new_data) != len(new_label):
+        raise RuntimeError(">> Error: new_data and new_label are not same length.")
 
     return data[0], label[0]
