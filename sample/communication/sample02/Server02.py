f32efc9409b865ae0c95c9b043c79abb0e57edff
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-11-02 11:28:05 +0900

    [fix] 指定した数のクライアントが接続するまで待機するように修正

--- sample/communication/sample02/Server02.py
+++ sample/communication/sample02/Server02.py
@@ -10,3 +10,15 @@ server_socket.bind(socket_address)
 server_socket.listen(client_size) # 接続可能なクライアント数は１
 print("サーバーが接続を待機中...")
 
+connections = []
+client_addresses = []
+
+while len(connections) < client_size:
+    connection, client_address = server_socket.accept()
+    connections.append(connection)
+    client_addresses.append(client_address)
+    print(f"クライアント {client_address} が接続しました。")
+
+for i in range(client_size):
+    connections[i].close()
+
