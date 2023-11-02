3ffeca1969084131f9c9d9f54fef0a8319fdf880
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-11-02 21:44:14 +0900

    [feat] 複数クライアントでも実行可能なSL完成（sleepでごまかしてる箇所がある）

--- src/SL_multiClient/Split_Client_Mulit.py
+++ src/SL_multiClient/Split_Client_Mulit.py
@@ -21,7 +21,24 @@ class BottomSL:
     def __init__(self, model, optimizer):
         self.model = model
         self.oprimizer = optimizer
+    
+    def bottom_forward(self, input):
+        smashed_data = []
+        smashed_data.append(self.model(input))
+        self.smashed_data = smashed_data
 
+        return smashed_data
+    
+    def bottom_backward(self, gradient):
+        self.smashed_data[0].backward(gradient)
+    
+    def zero_grad(self):
+        self.oprimizer.zero_grad()
+
+    def step(self):
+        self.oprimizer.step()
+
+############################################################################################################
 # サーバと接続
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 9876)
@@ -62,9 +79,8 @@ while send_start < len(compressed_label):
     send_end = send_start + chunck_size
     client_socket.send(compressed_label[send_start:send_end])
     send_start = send_end
+send_start = 0
 print('Sent labels!')
-
-client_socket.close()
 
 ############################################################################################################
 # 学習前の準備
@@ -72,10 +88,40 @@ BottomSL = BottomSL(bottom_model, optimizer)
 dataloader = DataLoader(train_data, batch_size=128, shuffle=False)
 
 # 学習開始
-# for i in range(epochs):
-#     print(f"---Epoch {i+1}/{epochs}---")
-#     count = 0
+for i in range(epochs):
+    print(f"---Epoch {i+1}/{epochs}---")
+    count = 0
 
-#     for data, ids in dataloader:
-#         count += 1
-#         print(f"Batch [{count}/{len(dataloader)}]")
+    for data, ids in dataloader:
+        count += 1
+        print(f"Batch [{count}/{len(dataloader)}]")
+
+        BottomSL.zero_grad()
+
+        time.sleep(0.5)
+        smashed_data = BottomSL.bottom_forward(data.float())
+        serialized_smashed_data = pickle.dumps(smashed_data)
+        compressed_smashed_data = zlib.compress(serialized_smashed_data) + end_flag
+
+        while send_start < len(compressed_smashed_data):
+            send_end = send_start + chunck_size
+            client_socket.send(compressed_smashed_data[send_start:send_end])
+            send_start = send_end
+        send_start = 0
+        
+        compressed_gradient = b""
+        while True:
+            chunk = client_socket.recv(chunck_size)
+            compressed_gradient += chunk
+            if compressed_gradient.endswith(end_flag):
+                compressed_gradient = compressed_gradient[:-len(end_flag)]
+                break
+        
+        uncompressed_gradient = zlib.decompress(compressed_gradient)
+        gradient = pickle.loads(uncompressed_gradient)
+
+        BottomSL.bottom_backward(gradient)
+        BottomSL.step()
+
+print('Training completed!')
+client_socket.close()
--- src/SL_multiClient/Split_Server_Multi.py
+++ src/SL_multiClient/Split_Server_Multi.py
@@ -1,3 +1,9 @@
+"""
+変数名のルール
+    - 最初にtrainが付く -> batch_training()で使う変数
+    - グローバル変数の場合は最初にg_が付く
+"""
+
 from torch import nn, optim
 from torch.utils.data import DataLoader
 import socket
@@ -11,16 +17,16 @@ epochs = 5
 input_size = 784
 hidden_size = [128, 64]
 output_size = 10
-client_num = 3
-chunk_size = 1024
 send_start = 0
 connections = []
 client_addresses = []
 labels = []
 dataloaders = []
 compressed_label = b""
-end_flag = b"END"
-train_flag = 0
+g_end_flag = b"END"
+g_chunk_size = 1024
+g_client_num = 2
+g_train_flag = 0
 
 # モデル
 models = [
@@ -40,32 +46,91 @@ models = [
 class TopSL:
     def __init__(self, model, optimizer):
         self.model = model
-        self.oprimizer = optimizer
+        self.optimizer = optimizer
+
+    def top_forward(self, smashed_data):
+        tensor = smashed_data[0].detach().requires_grad_(True)
+        preds = self.model(tensor)
+        self.tensor = tensor
+
+        return preds
+    
+    def top_backward(self):
+        grads = self.tensor.grad
+        
+        return grads
+
+    def zero_grads(self):
+        self.optimizer.zero_grad()
+
+    def step(self):
+        self.optimizer.step() 
 
 # マルチスレッドに関するクラス
 class myThread(threading.Thread):
-    def __init__(self, number, dataloader):
+    def __init__(self, number, connection, dataloader, TopSL):
         super(myThread, self).__init__()
         self.number = number
+        self.connection = connection
         self.dataloader = dataloader
+        self.TopSL = TopSL
     
     def run(self):
-        batch_training(self.number, self.dataloader)
+        batch_training(self.number, self.connection, self.dataloader, self.TopSL)
 
 # バッチ学習をするための関数（myThreadに呼び出される）
-def batch_training(client_number, dataloader):
-    global train_flag
-    global client_num
+def batch_training(client_number, connection, dataloader, TopSL):
+    global g_train_flag
+    global g_client_num
+    global g_chunk_size
+    global g_end_flag
+    global g_running_loss
+    global g_correct_preds
+    global g_total_preds
+
     batch_training_count = 1
     for label, id  in dataloader:
-        while (train_flag % client_num) != client_number:
+        while (g_train_flag % g_client_num) != client_number:
             pass
+
+        TopSL.zero_grads()
 
         print(f"Client {client_number}: {batch_training_count}/{len(dataloader)}")
         batch_training_count += 1
-        print(f"train_flag: {train_flag}")
+
+        train_compressed_smashed_data = b""
+        while True:
+            chunk = connection.recv(g_chunk_size)
+            train_compressed_smashed_data += chunk
+            if train_compressed_smashed_data.endswith(g_end_flag):
+                train_compressed_smashed_data = train_compressed_smashed_data[:-len(g_end_flag)]
+                break
+
+        train_uncompressed_smashed_data = zlib.decompress(train_compressed_smashed_data)
+        train_smashed_data = pickle.loads(train_uncompressed_smashed_data)
+
+        train_preds = TopSL.top_forward(train_smashed_data)
+        train_criterion = nn.NLLLoss()
+        train_loss = train_criterion(train_preds, label)
         
-        train_flag += 1
+        train_loss.backward()
+        train_grads = TopSL.top_backward()
+        TopSL.step()
+
+        train_serialized_grads = pickle.dumps(train_grads)
+        train_compressed_grads = zlib.compress(train_serialized_grads) + g_end_flag
+
+        train_start = 0
+        while train_start < len(train_compressed_grads):
+            train_end = train_start + g_chunk_size
+            connection.send(train_compressed_grads[train_start:train_end])
+            train_start = train_end
+        
+        g_running_loss += train_loss.item()
+        g_correct_preds += train_preds.max(1)[1].eq(label).sum().item()
+        g_total_preds += train_preds.size(0)
+
+        g_train_flag += 1
 
 ############################################################################################################
 # クライアントと接続
@@ -73,10 +138,10 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 9876)
 server_socket.bind(server_address)
 
-server_socket.listen(client_num)
+server_socket.listen(g_client_num)
 print('Waiting for connection...')
 
-while len(connections) < client_num:
+while len(connections) < g_client_num:
     connection, client_address = server_socket.accept()
     connections.append(connection)
     client_addresses.append(client_address)
@@ -86,11 +151,11 @@ print('All connected!')
 # 下位モデルを送信
 print('Sending bottom model...')
 serialized_bottom_model = pickle.dumps(models[0])
-compressed_bottom_model = zlib.compress(serialized_bottom_model) + end_flag
+compressed_bottom_model = zlib.compress(serialized_bottom_model) + g_end_flag
 
 for connection in connections:
     while send_start < len(compressed_bottom_model):
-        send_end = send_start + chunk_size
+        send_end = send_start + g_chunk_size
         connection.send(compressed_bottom_model[send_start:send_end])
         send_start = send_end
     send_start = 0
@@ -104,10 +169,10 @@ optimizer = optim.SGD(top_model.parameters(), lr=0.003)
 print('Receiving labels...')
 for connection in connections:
     while True:
-        chunk = connection.recv(chunk_size)
+        chunk = connection.recv(g_chunk_size)
         compressed_label += chunk
-        if compressed_label.endswith(end_flag):
-            compressed_label = compressed_label[:-len(end_flag)]
+        if compressed_label.endswith(g_end_flag):
+            compressed_label = compressed_label[:-len(g_end_flag)]
             break
     labels.append(pickle.loads(zlib.decompress(compressed_label)))
     compressed_label = b""
@@ -115,28 +180,34 @@ for connection in connections:
 for i in range(len(labels)):
     labels[i] = sorted(labels[i], key=lambda x: x[1])
 
-for connection in connections:
-    connection.close()
-
 ############################################################################################################
 # 学習前の準備
 TopSL = TopSL(top_model, optimizer)
 
-for i in range(client_num):
+for i in range(g_client_num):
     dataloaders.append(DataLoader(labels[i], batch_size=128, shuffle=False))
 
 # 学習開始
 for i in range(epochs):
     print(f"---Epoch {i+1}/{epochs}---")
+    g_running_loss = 0
+    g_correct_preds = 0
+    g_total_preds = 0
 
     threads = []
-    for i in range(client_num):
-        threads.append(myThread(i, dataloaders[i]))
-
-    for j in range(client_num):
+    for j in range(g_client_num):
+        threads.append(myThread(j, connections[j], dataloaders[j], TopSL))
+    for j in range(g_client_num):
         threads[j].start()
-    for j in range(client_num):
+
+    for j in range(g_client_num):
         threads[j].join()
-    train_flag = 0
 
+    g_train_flag = 0
 
+    print(f"Epoch {i+1} - Training loss: {g_running_loss/len(dataloaders[0])*g_client_num:.3f} - Training accuracy: {100*g_correct_preds/g_total_preds:.3f}\n")
+
+print("Finished training!")
+
+for connection in connections:
+    connection.close()
