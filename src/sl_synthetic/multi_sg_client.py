1854d1b11505622f16195ce0346883693d3c634e
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2023-12-11 21:26:18 +0900

    [fix] sendとreceiveをモジュール化

--- src/sl_synthetic/multi_sg_client.py
+++ src/sl_synthetic/multi_sg_client.py
@@ -9,6 +9,8 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import numpy as np
+import module.send_receive as sr
+import torch
 import random
 import time
 
@@ -16,37 +18,23 @@ import time
 class mnist():
     def __init__(self):
         self.train_dataset = dsets.MNIST(root='./data/', train=True, transform=transforms.ToTensor(), download=True)
-        self.test_dataset = dsets.MNIST(root='./data/', train=False, transform=transforms.ToTensor(), download=True)
-
+ 
         self.input_dims = 28 * 28
         self.num_classes = 10
         self.num_train = len(self.train_dataset)
-        self.num_test = len(self.test_dataset)
-
-        print(len(self.train_dataset))
-        print(len(self.test_dataset))
     
     def split_data_label(self, batch_size):
         self.train_data = []
         self.train_label = []
-        self.test_data = []
-        self.test_label = []
-
+ 
         train_ids = np.arange(self.num_train)
         np.random.shuffle(train_ids)
         for i in range(self.num_train):
             self.train_data.append(self.train_dataset[train_ids[i]][0])
             self.train_label.append(self.train_dataset[train_ids[i]][1])
 
-        test_ids = np.arange(self.num_test)
-        np.random.shuffle(test_ids)
-        for i in range(self.num_test):
-            self.test_data.append(self.test_dataset[test_ids[i]][0])
-            self.test_label.append(self.test_dataset[test_ids[i]][1])
-        
         self.train_loader = DataLoader(self.train_data, batch_size=batch_size, shuffle=False)
-        self.test_loader = DataLoader(self.test_data, batch_size=batch_size, shuffle=False)
-
+ 
 ############################################################################################################
 class model(nn.Module):
     def __init__(self, input_dims, split_layer_size):
@@ -73,26 +61,28 @@ class model(nn.Module):
 
         self.optimizer = optim.Adam(self.layer1.parameters(), lr=0.001)
         self.sg_optimizer = optim.Adam(self.sg_model.parameters(), lr=0.001)
-        self.sg_optimizers = []
-        self.sg_optimizers.append(optim.Adam(self.sg_layer1.parameters(), lr=0.001))
-        self.sg_optimizers.append(optim.Adam(self.sg_layer2.parameters(), lr=0.001))
         self.sg_criterion = nn.MSELoss()
     
     def bottom_forward(self, data):
         self.optimizer.zero_grad()
+        self.data = data
         self.smashed_data = self.layer1(data)
-        mean_output = self.smashed_data.mean(dim=0)
-        grad = self.sg_layer1(mean_output)
-        grad = grad.reshape(self.split_layer_size, self.input_dims)
-        bias = self.sg_layer2(mean_output)
-        return self.smashed_data, grad, bias
-    
-    def sg_backward(self):
-        return 0
+        if sg_flag == True:
+            mean_output = self.smashed_data.mean(dim=0)
+            grad = self.sg_layer1(mean_output)
+            grad = grad.reshape(self.split_layer_size, self.input_dims)
+            bias = self.sg_layer2(mean_output)
+            return self.smashed_data, grad, bias
+        else:
+            return self.smashed_data
     
     def bottom_backward(self, grads):
-        self.smashed_data.backward(grads, retain_graph=True)
+        print(self.optimizer.param_groups[0]['params'][1].grad)
+        self.smashed_data.backward(gradient=grads, retain_graph=True)
+        print(self.optimizer.param_groups[0]['params'][1].grad)
         self.optimizer.step()
+
+        time.sleep(5)
 
     def model_test(self, test_loader):
         self.test_loader = test_loader
@@ -110,6 +100,8 @@ end_message = b"END"
 
 hidden_size = [128, 64]
 
+sg_flag = True
+
 ############################################################################################################
 #サーバと接続
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -123,39 +115,9 @@ m.split_data_label(batch_size)
 
 # 学習データの正解ラベルを送信
 print('Sending train label...')
-serialized_train_label = pickle.dumps(m.train_label)
-compressed_train_label = zlib.compress(serialized_train_label) + end_message
-while True:
-    receive_message = client_socket.recv(chunk_size)
-    if receive_message == start_message:
-        break
-while send_progress < len(compressed_train_label):
-    send_progress += chunk_size
-    client_socket.send(compressed_train_label[send_progress-chunk_size:send_progress])
-while True:
-    receive_message = client_socket.recv(chunk_size)
-    if receive_message == end_message:
-        break
+sr.send(client_socket, m.train_label)
 print('Train label sent!')
-send_progress = 0
 
-# テストデータの正解ラベルを送信
-print('Sending test label...')
-serialized_test_label = pickle.dumps(m.test_label)
-compressed_test_label = zlib.compress(serialized_test_label) + end_message
-while True:
-    receive_message = client_socket.recv(chunk_size)
-    if receive_message == start_message:
-        break
-while send_progress < len(compressed_test_label):
-    send_progress += chunk_size
-    client_socket.send(compressed_test_label[send_progress-chunk_size:send_progress])
-while True:
-    receive_message = client_socket.recv(chunk_size)
-    if receive_message == end_message:
-        break
-print('Test label sent!')
-send_progress = 0
 
 ###########################################################################################################
 # モデルの定義
@@ -163,126 +125,59 @@ model = model(m.input_dims, hidden_size[0])
 
 for i in range(epoch):
     print(f"---Epoch {i+1}/{epoch}---")
-    count = 0
 
     for data in m.train_loader:
-        grad_loss = 0
-        count += 1
-        print(f"---Batch {count}/{len(m.train_loader)}---")
+        grad_loss = 0.0
         false_grad_list = []
         true_grad_list = []
 
         # bottom modelのforward
         data = data.view(-1, m.input_dims)
-        smashed_data, sg_grad, sg_bias = model.bottom_forward(data)
-        false_grad_list.append(sg_grad)
-        false_grad_list.append(sg_bias)
+        if sg_flag == True:
+            smashed_data, sg_grad, sg_bias = model.bottom_forward(data)
+            false_grad_list.append(sg_grad)
+            false_grad_list.append(sg_bias)
+        else:
+            smashed_data = model.bottom_forward(data)
 
         # send smashed data
-        serialized_smashed_data = pickle.dumps(smashed_data)
-        compressed_smashed_data = zlib.compress(serialized_smashed_data) + end_message
-        while True:
-            receive_message = client_socket.recv(chunk_size)
-            if receive_message == start_message:
-                break
-        while send_progress < len(compressed_smashed_data):
-            send_progress += chunk_size
-            client_socket.send(compressed_smashed_data[send_progress-chunk_size:send_progress])
-        while True:
-            receive_message = client_socket.recv(chunk_size)
-            if receive_message == end_message:
-                break
-        send_progress = 0
+        sr.send(client_socket, smashed_data)
 
         # sg modelを用いた更新
-        for j in range(len(model.optimizer.param_groups[0]['params'])):
-            model.optimizer.param_groups[0]['params'][j].grad = false_grad_list[j].detach()
-        model.optimizer.step()
+        if sg_flag == True:
+            for j in range(len(model.optimizer.param_groups[0]['params'])):
+                model.optimizer.param_groups[0]['params'][j].grad = false_grad_list[j].detach()
+            model.optimizer.step() # ここでパラメータが更新されたかを確認する
 
         # receive grads
-        client_socket.send(start_message)
-        compressed_grads = b""
-        while True:
-            chunk = client_socket.recv(chunk_size)
-            compressed_grads += chunk
-            if compressed_grads.endswith(end_message):
-                compressed_grads = compressed_grads[:-len(end_message)]
-                break
-        client_socket.send(end_message)
-        uncompressed_grads = zlib.decompress(compressed_grads)
-        grads = pickle.loads(uncompressed_grads)
+        grads = sr.receive(client_socket)
 
         # bottom modelのbackward
         model.bottom_backward(grads)
 
         # sg modelの更新
-        for j in range(len(model.optimizer.param_groups[0]['params'])):
-            true_grad_list.append(model.optimizer.param_groups[0]['params'][j].grad)
-            grad_loss += model.sg_criterion(false_grad_list[j], true_grad_list[j])
-        grad_loss /= len(model.optimizer.param_groups[0]['params'])
-        grad_loss.backward()
-        model.sg_optimizer.step()
+        if sg_flag == True:
+            for j in range(len(model.optimizer.param_groups[0]['params'])):
+                true_grad_list.append(model.optimizer.param_groups[0]['params'][j].grad)
+                print(false_grad_list[j])
+                print(true_grad_list[j])
+                loss = model.sg_criterion(false_grad_list[j], true_grad_list[j])
+                print(f"loss: {loss}")
+                grad_loss += loss
+            grad_loss /= len(model.sg_optimizer.param_groups[0]['params'])
+            grad_loss.backward()
+            model.sg_optimizer.step()
+            print(f"grad_loss: {grad_loss}")
     
     # サーバにモデルを送信
     print('Sending model...')
-    layer1_param = model.optimizer.param_groups[0]['params'][0].detach()
-    layer1_bias = model.optimizer.param_groups[0]['params'][1].detach()
-    serialized_layer1_param = pickle.dumps(layer1_param)
-    serialized_layer1_bias = pickle.dumps(layer1_bias)
-    compressed_layer1_param = zlib.compress(serialized_layer1_param) + end_message
-    compressed_layer1_bias = zlib.compress(serialized_layer1_bias) + end_message
-    while True:
-        receive_message = client_socket.recv(chunk_size)
-        if receive_message == start_message:
-            break
-    while send_progress < len(compressed_layer1_param):
-        send_progress += chunk_size
-        client_socket.send(compressed_layer1_param[send_progress-chunk_size:send_progress])
-    while True:
-        receive_message = client_socket.recv(chunk_size)
-        if receive_message == end_message:
-            break
-    send_progress = 0
-    while True:
-        receive_message = client_socket.recv(chunk_size)
-        if receive_message == start_message:
-            break
-    while send_progress < len(compressed_layer1_bias):
-        send_progress += chunk_size
-        client_socket.send(compressed_layer1_bias[send_progress-chunk_size:send_progress])
-    while True:
-        receive_message = client_socket.recv(chunk_size)
-        if receive_message == end_message:
-            break
-    send_progress = 0
+    sr.send(client_socket, model.optimizer.param_groups[0]['params'][0].detach())
+    sr.send(client_socket, model.optimizer.param_groups[0]['params'][1].detach())
 
     # サーバからモデルを受信
     print('Receiving model...')
-    compressed_layer1_param = b""
-    compressed_layer1_bias = b""
-    client_socket.send(start_message)
-    while True:
-        chunk = client_socket.recv(chunk_size)
-        compressed_layer1_param += chunk
-        if compressed_layer1_param.endswith(end_message):
-            compressed_layer1_param = compressed_layer1_param[:-len(end_message)]
-            break
-    client_socket.send(end_message)
-    client_socket.send(start_message)
-    while True:
-        chunk = client_socket.recv(chunk_size)
-        compressed_layer1_bias += chunk
-        if compressed_layer1_bias.endswith(end_message):
-            compressed_layer1_bias = compressed_layer1_bias[:-len(end_message)]
-            break
-    client_socket.send(end_message)
-    uncompressed_layer1_param = zlib.decompress(compressed_layer1_param)
-    uncompressed_layer1_bias = zlib.decompress(compressed_layer1_bias)
-    layer1_param = pickle.loads(uncompressed_layer1_param)
-    layer1_bias = pickle.loads(uncompressed_layer1_bias)
-    model.optimizer.param_groups[0]['params'][0].data = layer1_param
-    model.optimizer.param_groups[0]['params'][1].data = layer1_bias
-
+    model.optimizer.param_groups[0]['params'][0].data = sr.receive(client_socket)
+    model.optimizer.param_groups[0]['params'][1].data = sr.receive(client_socket)
 
 client_socket.close()
 ############################################################################################################
--- src/sl_synthetic/multi_sg_server.py
+++ src/sl_synthetic/multi_sg_server.py
@@ -1,10 +1,13 @@
 from torch import nn, optim
 from torch.utils.data import DataLoader
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
 import torch
 import socket
 import pickle
 import zlib
 import time
+import module.send_receive as sr
 
 ############################################################################################################
 class model(nn.Module):
@@ -52,17 +55,7 @@ def learning(model, train_iter, connection):
     label = next(train_iter)
 
     # receive smashed data
-    compressed_smashed_data = b""
-    connection.send(start_message)
-    while True:
-        chunk = connection.recv(chunk_size)
-        compressed_smashed_data += chunk
-        if compressed_smashed_data.endswith(end_message):
-            compressed_smashed_data = compressed_smashed_data[:-len(end_message)]
-            break
-    connection.send(end_message)
-    uncompressed_smashed_data = zlib.decompress(compressed_smashed_data)
-    smashed_data = pickle.loads(uncompressed_smashed_data)
+    smashed_data = sr.receive(connection)
 
     # top modelのforward
     output = model.top_forward(smashed_data)
@@ -71,26 +64,13 @@ def learning(model, train_iter, connection):
     loss, grads = model.top_backward(label)
 
     # send grads
-    serialized_grads = pickle.dumps(grads)
-    compressed_grads = zlib.compress(serialized_grads) + end_message
-    while True:
-        receive_message = connection.recv(chunk_size)
-        if receive_message == start_message:
-            break
-    while g_send_progress < len(compressed_grads):
-        g_send_progress += chunk_size
-        connection.send(compressed_grads[g_send_progress-chunk_size:g_send_progress])
-    while True:
-        receive_message = connection.recv(chunk_size)
-        if receive_message == end_message:
-            break
-    g_send_progress = 0
+    sr.send(connection, grads)
 
     return loss
 
 ##########################################################################################################
 g_send_progress = 0
-num_client = 2
+num_client = 1
 epoch = 2
 output_size = 10
 chunk_size = 1024
@@ -103,9 +83,7 @@ hidden_size = [128, 64]
 connection_list = []
 client_address_list = []
 train_label_list = []
-test_label_list = []
 train_loader_list = []
-test_loader_list = []
 
 ##########################################################################################################
 # クライアントと接続
@@ -126,45 +104,17 @@ print('All connected!')
 # receive train label
 print('Receiving train label...')
 for connection in connection_list:
-    compressed_train_label = b""
-    connection.send(start_message)
-    while True:
-        chunk = connection.recv(chunk_size)
-        compressed_train_label += chunk
-        if compressed_train_label.endswith(end_message):
-            compressed_train_label = compressed_train_label[:-len(end_message)]
-            break
-    connection.send(end_message)
-    uncompressed_train_label = zlib.decompress(compressed_train_label)
-    train_label = pickle.loads(uncompressed_train_label)
+    train_label = sr.receive(connection)
     train_label_list.append(train_label)
 print('Train label received!')
-
-# receive test label
-print('Receiving test label...')
-for connection in connection_list:
-    compressed_test_label = b""
-    connection.send(start_message)
-    while True:
-        chunk = connection.recv(chunk_size)
-        compressed_test_label += chunk
-        if compressed_test_label.endswith(end_message):
-            compressed_test_label = compressed_test_label[:-len(end_message)]
-            break
-    connection.send(end_message)
-    uncompressed_test_label = zlib.decompress(compressed_test_label)
-    test_label = pickle.loads(uncompressed_test_label)
-    test_label_list.append(test_label)
-print('Test label received!')
 
 # データローダの作成
 for train_label in train_label_list:
     train_loader = DataLoader(train_label, batch_size=128, shuffle=False)
     train_loader_list.append(train_loader)
 
-for test_label in test_label_list:
-    test_loader = DataLoader(test_label, batch_size=128, shuffle=False)
-    test_loader_list.append(test_loader)
+test_dataset = dsets.MNIST(root='./data', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 ##########################################################################################################
 # モデルの作成
@@ -173,9 +123,6 @@ model = model(hidden_size, output_size)
 train_start_time = time.time()
 for i in range(epoch):
     # print(f"---Epoch {i+1}/{epoch}---")
-    running_loss = 0.0
-    correct_preds = 0
-    total_preds = 0
     train_iter_list = []
     test_iter_list = []
     layer1_param = torch.zeros(hidden_size[0], 28*28)
@@ -184,9 +131,6 @@ for i in range(epoch):
     for train_loader in train_loader_list:
         train_iter = iter(train_loader)
         train_iter_list.append(train_iter)
-    for test_loader in test_loader_list:
-        test_iter = iter(test_loader)
-        test_iter_list.append(test_iter)
 
     for j in range(len(train_loader_list[0])):
         loss = 0.0
@@ -196,84 +140,37 @@ for i in range(epoch):
     
     for j in range(num_client):
         # receive layer1 param
-        print(f"Receiving layer1 param from {client_address_list[j]}...")
-        compressed_layer1_param = b""
-        connection_list[j].send(start_message)
-        while True:
-            chunk = connection_list[j].recv(chunk_size)
-            compressed_layer1_param += chunk
-            if compressed_layer1_param.endswith(end_message):
-                compressed_layer1_param = compressed_layer1_param[:-len(end_message)]
-                break
-        connection_list[j].send(end_message)
-        uncompressed_layer1_param = zlib.decompress(compressed_layer1_param)
-        bottom_layer1_param = pickle.loads(uncompressed_layer1_param)
-        print(bottom_layer1_param.shape)
-        print(layer1_param.shape)
-        layer1_param += pickle.loads(uncompressed_layer1_param)
-
+        layer1_param += sr.receive(connection_list[j])
         # receive layer1 bias
-        compressed_layer1_bias = b""
-        connection_list[j].send(start_message)
-        while True:
-            chunk = connection_list[j].recv(chunk_size)
-            compressed_layer1_bias += chunk
-            if compressed_layer1_bias.endswith(end_message):
-                compressed_layer1_bias = compressed_layer1_bias[:-len(end_message)]
-                break
-        connection_list[j].send(end_message)
-        uncompressed_layer1_bias = zlib.decompress(compressed_layer1_bias)
-        layer1_bias += pickle.loads(uncompressed_layer1_bias)
-    
+        layer1_bias += sr.receive(connection_list[j])
+
     # layer1 paramの平均を取る
-    print("Averaging layer1 param...")
     layer1_param /= num_client
-    print("Averaging layer1 bias...")
     layer1_bias /= num_client
 
-    # layer1 paramを送信
-    serialized_layer1_param = pickle.dumps(layer1_param)
-    compressed_layer1_param = zlib.compress(serialized_layer1_param) + end_message
+    # layer1 param bias を送信
     for j in range(num_client):
-        print(f"Sending layer1 param to {client_address_list[j]}...")
-        while True:
-            receive_message = connection_list[j].recv(chunk_size)
-            if receive_message == start_message:
-                break
-        while g_send_progress < len(compressed_layer1_param):
-            g_send_progress += chunk_size
-            connection_list[j].send(compressed_layer1_param[g_send_progress-chunk_size:g_send_progress])
-        while True:
-            receive_message = connection_list[j].recv(chunk_size)
-            if receive_message == end_message:
-                break
-        g_send_progress = 0
-    # layer1 biasを送信
-    for j in range(num_client):
-        print(f"Sending layer1 bias to {client_address_list[j]}...")
-        serialized_layer1_bias = pickle.dumps(layer1_bias)
-        compressed_layer1_bias = zlib.compress(serialized_layer1_bias) + end_message
-        while True:
-            receive_message = connection_list[j].recv(chunk_size)
-            if receive_message == start_message:
-                break
-        while g_send_progress < len(compressed_layer1_bias):
-            g_send_progress += chunk_size
-            connection_list[j].send(compressed_layer1_bias[g_send_progress-chunk_size:g_send_progress])
-        while True:
-            receive_message = connection_list[j].recv(chunk_size)
-            if receive_message == end_message:
-                break
-        g_send_progress = 0
+        sr.send(connection_list[j], layer1_param)
+        sr.send(connection_list[j], layer1_bias)
     
     model.bottom_model[0].weight.data = layer1_param
     model.bottom_model[0].bias.data = layer1_bias
 
     # test
-    
+    correct = 0
+    total = 0
+    for data, label in test_loader:
+        data = data.view(-1, 28*28)
+        test_smashed_data = model.bottom_model(data)
+        output = model.top_model(test_smashed_data)
+        _, predicted = torch.max(output.data, 1)
+        total += label.size(0)
+        correct += (predicted == label).sum().item()
+    print(f"Epoch {i+1}/{epoch}: {correct/total*100}")
+
 
 train_end_time = time.time()
-print(f"train time: {train_end_time - train_start_time}")
+print(f"train time: {train_end_time - train_start_time}%")
 
 for connection in connection_list:
     connection.close()
